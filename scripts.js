1cfd40fc1fbafae0cd52b5faedd209650e12fa5a
Meraj <haquedot@gmail.com>
2024-11-11 15:35:08 +0530

    created contact and testimonial sections

--- scripts.js
+++ scripts.js
@@ -20,16 +20,36 @@ $(document).ready(function(){
         loop: true,
         margin: 10,
         nav: true,
-        dots: false, // Remove dots
+        dots: false,
         responsive: {
             0: {
-                items: 1 // 1 item on mobile
+                items: 1 
             },
             600: {
-                items: 2 // 2 items on tablets
+                items: 2 
             },
             1000: {
-                items: 1 // 1 item on larger screens
+                items: 1 
+            }
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".testimonials-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1024: {
+                items: 2.7
             }
         }
     });
